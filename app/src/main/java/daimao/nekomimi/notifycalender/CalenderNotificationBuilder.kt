8a729c17a07f2bd49f6a4b7ac57ee93f98e62980
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2025-02-26 23:47:26 +0900

    通知アイコンで日付を表示する

--- app/src/main/java/daimao/nekomimi/notifycalender/CalenderNotificationBuilder.kt
+++ app/src/main/java/daimao/nekomimi/notifycalender/CalenderNotificationBuilder.kt
@@ -28,8 +28,9 @@ fun postNotification(context: Context) {
     if (!PermissionHelper.hasNotifyPermission(context)) {
         return
     }
+
     val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setSmallIcon(getNotificationIcon(LocalDate.now().dayOfMonth))
         .setContentTitle(formatDate())
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         .setAutoCancel(false)
@@ -56,6 +57,42 @@ fun formatDate(): String {
     return LocalDate.now().format(formatter)
 }
 
+fun getNotificationIcon(day: Int): Int {
+    return when (day) {
+        1 -> R.drawable.notification_icon_01
+        2 -> R.drawable.notification_icon_02
+        3 -> R.drawable.notification_icon_03
+        4 -> R.drawable.notification_icon_04
+        5 -> R.drawable.notification_icon_05
+        6 -> R.drawable.notification_icon_06
+        7 -> R.drawable.notification_icon_07
+        8 -> R.drawable.notification_icon_08
+        9 -> R.drawable.notification_icon_09
+        10 -> R.drawable.notification_icon_10
+        11 -> R.drawable.notification_icon_11
+        12 -> R.drawable.notification_icon_12
+        13 -> R.drawable.notification_icon_13
+        14 -> R.drawable.notification_icon_14
+        15 -> R.drawable.notification_icon_15
+        16 -> R.drawable.notification_icon_16
+        17 -> R.drawable.notification_icon_17
+        18 -> R.drawable.notification_icon_18
+        19 -> R.drawable.notification_icon_19
+        20 -> R.drawable.notification_icon_20
+        21 -> R.drawable.notification_icon_21
+        22 -> R.drawable.notification_icon_22
+        23 -> R.drawable.notification_icon_23
+        24 -> R.drawable.notification_icon_24
+        25 -> R.drawable.notification_icon_25
+        26 -> R.drawable.notification_icon_26
+        27 -> R.drawable.notification_icon_27
+        28 -> R.drawable.notification_icon_28
+        29 -> R.drawable.notification_icon_29
+        30 -> R.drawable.notification_icon_30
+        31 -> R.drawable.notification_icon_31
+        else -> R.drawable.notification_icon_01 // Default icon for invalid days
+    }
+}
 
 
 
